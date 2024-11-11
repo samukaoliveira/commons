0ef288c0acaa34e63f2c01186873f44978c227c8
Samuel <mucabatera@gmail.com>
2024-11-11 18:08:53 -0300

    Commit inicial

---
+++ src/main/java/br/com/sti/commons/constants/Constants.java
@@ -0,0 +1,9 @@
+package br.com.sti.commons.constants;
+
+public class Constants {
+
+    public static final String EXG_NAME_MARKETPLACE = "maketplace.direct";
+    public static final String QUEUE_PRODUCT_LOG = "product.log";
+    public static final String RK_PRODUCT_LOG = "product.log";
+    
+}
---
+++ src/test/java/br/com/sti/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sti.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
